05ecd230edccec0634f033ed1cadad23e0fab85d
Stelios Georgiadis <steliosgeor7@gmail.com>
2018-06-07 01:06:08 +0300

    Second Commit CHOCOOOOOOOOO!

---
+++ src/main/java/org/ebayopensource/fidouaf/stats/History_Auth_Object.java
@@ -0,0 +1,16 @@
+package org.ebayopensource.fidouaf.stats;
+
+import java.sql.Timestamp;
+
+public class History_Auth_Object {
+	
+	public String AAID;
+	public String KeyID;
+	public String deviceId;
+	public String username;
+	public String status;
+	public int auth_time  /*new Timestamp(System.currentTimeMillis()); */ ;
+	
+	
+	
+}
